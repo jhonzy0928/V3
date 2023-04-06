437e547da8e68b0d26b6e8c80fff243e51e7b93c
zhu young <1824833631@qq.com>
2023-04-06 11:26:08 +0800

    V3

---
+++ src/UniswapV3Pool.sol
@@ -0,0 +1,222 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+import "./lib/Tick.sol";
+import "./lib/Position.sol";
+import "../lib/solmate/src/tokens/ERC20.sol";
+
+interface IERC20 {
+    function balanceOf(address) external returns (uint256);
+
+    function transfer(address to, uint256 amount) external;
+}
+
+interface IUniswapV3SwapCallback {
+    /// @notice Called to `msg.sender` after executing a swap via IUniswapV3Pool#swap.
+    /// @dev In the implementation you must pay the pool tokens owed for the swap.
+    /// The caller of this method must be checked to be a UniswapV3Pool deployed by the canonical UniswapV3Factory.
+    /// amount0Delta and amount1Delta can both be 0 if no tokens were swapped.
+    /// @param amount0Delta The amount of token0 that was sent (negative) or must be received (positive) by the pool by
+    /// the end of the swap. If positive, the callback must send that amount of token0 to the pool.
+    /// @param amount1Delta The amount of token1 that was sent (negative) or must be received (positive) by the pool by
+    /// the end of the swap. If positive, the callback must send that amount of token1 to the pool.
+    /// @param data Any data passed through by the caller via the IUniswapV3PoolActions#swap call
+    function uniswapV3SwapCallback(
+        int256 amount0Delta,
+        int256 amount1Delta,
+        bytes calldata data
+    ) external;
+}
+
+interface IUniswapV3MintCallback {
+    /// @notice Called to `msg.sender` after minting liquidity to a position from IUniswapV3Pool#mint.
+    /// @dev In the implementation you must pay the pool tokens owed for the minted liquidity.
+    /// The caller of this method must be checked to be a UniswapV3Pool deployed by the canonical UniswapV3Factory.
+    /// @param amount0Owed The amount of token0 due to the pool for the minted liquidity
+    /// @param amount1Owed The amount of token1 due to the pool for the minted liquidity
+    ///  Any data passed through by the caller via the IUniswapV3PoolActions#mint call
+    function uniswapV3MintCallback(
+        uint256 amount0Owed,
+        uint256 amount1Owed
+    ) external;
+}
+
+contract UniswapV3Pool is ERC20 {
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
+
+    mapping(address => uint256) private _balances;
+
+    error InvalidTickRange();
+    error ZeroLiquidity();
+    error InsufficientInputAmount();
+    error transferFailed();
+
+    event Mint(
+        address indexed sender,
+        address indexed owner,
+        int24 lowerTick,
+        int24 upperTick,
+        uint256 amount,
+        uint256 amount0,
+        uint256 amount1
+    );
+    event Swap(
+        address indexed sender,
+        address indexed recipient,
+        int256 amount0,
+        int256 amount1,
+        uint160 sqrtPriceX96,
+        uint128 liquidity,
+        int24 tick
+    );
+
+    int24 internal constant MIN_TICK = -887272;
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+    //pool tokens immutable
+    address public immutable token0;
+    address public immutable token1;
+
+    struct Slot0 {
+        uint160 sqrtPriceX96;
+        int24 tick;
+    }
+    Slot0 public slot0;
+    struct CallbackData {
+        address token0;
+        address token1;
+        address payer;
+    }
+
+    uint128 public liquidity;
+
+    mapping(int24 => Tick.Info) public ticks;
+    mapping(bytes32 => Position.Info) public positions;
+
+    //在构造函数中，我们初始化了不可变的 token 地址、现在的价格和对应的 tick。
+    constructor(
+        address token0_,
+        address token1_,
+        uint160 sqrtPriceX96,
+        int24 tick
+    ) ERC20("pair", "zy", 18) {
+        token0 = token0_;
+        token1 = token1_;
+
+        slot0 = Slot0({sqrtPriceX96: sqrtPriceX96, tick: tick});
+    }
+
+    function balance0() private view returns (uint256) {
+        (bool success, bytes memory data) = token0.staticcall(
+            abi.encodeWithSelector(IERC20.balanceOf.selector, address(this))
+        );
+        require(success && data.length >= 32);
+        return abi.decode(data, (uint256));
+    }
+
+    function balance1() private view returns (uint256) {
+        (bool success, bytes memory data) = token1.staticcall(
+            abi.encodeWithSelector(IERC20.balanceOf.selector, address(this))
+        );
+        require(success && data.length >= 32);
+        return abi.decode(data, (uint256));
+    }
+
+    // function _safeTransfer(address token, address to, uint256 value) private {
+    //     //可以调用其他合约函数
+    //     (bool success, bytes memory data) = token.call(
+    //         abi.encodeWithSignature("transfer(address,uint256)", to, value)
+    //     );
+    //     if (!success || (data.length != 0 && !abi.decode(data, (bool)))) {
+    //         revert transferFailed();
+    //     }
+    // }
+
+    function mint(
+        address owner, //token 所有者的地址，来识别是谁提供的流动性
+        int24 lowerTick, //上界和下界的 tick，来设置价格区间的边界；
+        int24 upperTick,
+        uint128 amount, //希望提供的流动性的数量
+        bytes calldata data //传入不会被函数本身使用的函数
+    ) external returns (uint256 amount0, uint256 amount1) {
+        if (
+            lowerTick >= upperTick ||
+            lowerTick < MIN_TICK ||
+            upperTick > MAX_TICK
+        ) revert InvalidTickRange();
+        if (amount == 0) revert ZeroLiquidity();
+        //添加tick和position信息
+        ticks.update(lowerTick, amount);
+        ticks.update(upperTick, amount);
+
+        Position.Info storage position = positions.get(
+            owner,
+            lowerTick,
+            upperTick
+        );
+        position.update(amount);
+
+        uint256 balance0Before;
+        uint256 balance1Before;
+        amount0 = 0.998976618347425280 ether;
+        amount1 = 5000 ether;
+
+        if (amount0 > 0) balance0Before = balance0();
+        if (amount1 > 0) balance1Before = balance1();
+        //这个callback为啥会在外面实现 是因为带了callback特殊？
+        IUniswapV3MintCallback(msg.sender).uniswapV3MintCallback(
+            amount0,
+            amount1,
+            data
+        );
+
+        if (amount0 > 0 && balance0Before + amount0 < balance0())
+            revert InsufficientInputAmount();
+        if (amount1 > 0 && balance1Before + amount1 < balance1())
+            revert InsufficientInputAmount();
+        emit Mint(
+            msg.sender,
+            owner,
+            lowerTick,
+            upperTick,
+            amount,
+            amount0,
+            amount1
+        );
+    }
+
+    //recipient==提出token的接受者
+    function swap(
+        address recipient,
+        bytes calldata data
+    ) public returns (int256 amount0, int256 amount1) {
+        int24 nextTick = 85184;
+        uint160 nextPrice = 5604469350942327889444743441197;
+
+        amount0 = -0.008396714242162444 ether;
+        amount1 = 42 ether;
+        //更新现在的 tick 和对应的 sqrtP：
+        (slot0.tick, slot0.sqrtPriceX96) = (nextTick, nextPrice);
+        //合约把对应的 token 发送给 recipient 并且让调用者将需要的 token 转移到本合约：
+        IERC20(token0).transfer(recipient, uint256(-amount0));
+
+        uint256 balance1Before = balance1();
+        IUniswapV3SwapCallback(msg.sender).uniswapV3SwapCallback(
+            amount0,
+            amount1,
+            data
+        );
+        if (balance1Before + uint256(amount1) < balance1())
+            revert InsufficientInputAmount();
+        emit Swap(
+            msg.sender,
+            recipient,
+            amount0,
+            amount1,
+            slot0.sqrtPriceX96,
+            liquidity,
+            slot0.tick
+        );
+    }
+}
